44279ea02a0813b793631295cd4c9ddb47d44ca2
jpaguio <jpaguio@US1WL53WPXW1>
2016-12-14 15:49:27 -0600

    Can't register dll

---
+++ SimpleCalc/SimpleCalc/Calc.vb
@@ -0,0 +1,31 @@
+﻿Imports System
+
+Public Class Calc
+    Private m_numberOne As Integer = 0
+    Private m_numberTwo As Integer = 0
+
+    Public Property NumberOne() As Integer
+        Get
+            Return m_numberOne
+        End Get
+        Set(value As Integer)
+            m_numberOne = value
+        End Set
+    End Property
+
+    Public Property NumberTwo() As Integer
+        Get
+            Return m_numberTwo
+        End Get
+        Set(value As Integer)
+            m_numberTwo = value
+        End Set
+    End Property
+
+    Public ReadOnly Property OneAddTwo()
+        Get
+            Return m_numberOne + m_numberTwo
+        End Get
+    End Property
+End Class
+
---
+++ SimpleCalc/SimpleCalc/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("SimpleCalc")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Technip USA")> 
+<Assembly: AssemblyProduct("SimpleCalc")> 
+<Assembly: AssemblyCopyright("Copyright © Technip USA 2016")> 
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(True)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("e101e4a5-601f-45f6-b3cd-dbddf0af2020")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
